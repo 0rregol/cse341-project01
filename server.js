766955f62c6073ea487f3f3ad705006c567428d2
0rregol <orr2404@byui.edu>
2025-11-07 10:32:05 -0300

    20251107_01

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use('/', require('./routes'));
-app.listen(port, () => {console.log(`Server running on port ${port}`);});
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+        } else
+            {
+app.listen(port, () => {console.log(`database is listening and node running on port ${port}`);});
+ }});
